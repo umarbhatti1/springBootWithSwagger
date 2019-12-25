80ff48a6b0a38b39b45c0fee60af211ab387907a
umarbhatti <umar.hakeem@clickchain.com>
2019-12-25 05:31:16 +0500

    fixing

--- src/main/java/com/prototype/model/User.java
+++ src/main/java/com/prototype/model/User.java
@@ -1,15 +1,40 @@
 package com.prototype.model;
 
-import lombok.*;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@Builder
-@NoArgsConstructor
 public class User {
     private String name;
     private String id;
     private int age;
     private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
---
+++ src/test/java/com/prototype/UserProfileTests.java
@@ -0,0 +1,13 @@
+package com.prototype;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class UserProfileTests {
+
+    @Test
+    public void check(){
+        assertTrue(false);
+    }
+}
